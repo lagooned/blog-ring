c1b23ebb17f5b095721853eb945bdd6b21ef32c5
Jared Engler <jared.lite@gmail.com>
2022-03-05 00:02:03 -0600

    fix sorting

--- ring.sh
+++ ring.sh
@@ -3,6 +3,6 @@ echo -e "$(cat ~/.br-follows | tr ' ' '\n' | \
         curl -s $url?$RANDOM \
             | sed "s/^\([0-9]*\) /"$u": \1 /g" ; done)" \
             | sort -nr -k2 \
-            | awk '{$2 = "\b"; print $0}' \
-            | grep -E --color=always "\@$USERNAME|$"
+            | grep -E --color=always "\@$USERNAME|$" \
+            | awk -F " " '{for(i=1;i<=NF;i++)if(i!=2)f=f?f FS $i:$i;print f;f=""}'
 
