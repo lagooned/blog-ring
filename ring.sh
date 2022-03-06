ba745e5709f96f4276a6edb40577042778c5a89a
lagooned <jared.lite@gmail.com>
2022-03-06 16:35:42 -0600

    use wget, seems less affected by caching

--- ring.sh
+++ ring.sh
@@ -1,5 +1,5 @@
 echo -e "$(cat ~/.br-follows | tr ' ' '\n' | while read u; do read url;
-  curl -s $url?$RANDOM | sed "s/^\([0-9]*\) /"$u": \1 /g" ; done)" \
+  wget -qO- $url?$RANDOM | sed "s/^\([0-9]*\) /"$u": \1 /g" ; done)" \
   | sort -nr -k2 \
   | grep -E --color=always "\@$USERNAME|$" \
   | awk -F " " '{for(i=1;i<=NF;i++)if(i!=2)f=f?f FS $i:$i;print f;f=""}'
